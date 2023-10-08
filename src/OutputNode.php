f5ece9bbd9611c83415e04da67d8b5890a9454c3
Barták Iván <bartakivan@gmail.com>
2023-10-08 20:00:58 +0200

    Addet output buffer to output node

--- src/OutputNode.php
+++ src/OutputNode.php
@@ -13,11 +13,11 @@ abstract class OutputNode extends Node{
         }
         $stopColor = parent::calculate($cycle);
         if($stopColor === null && isset($cycle)){
-            $this->lastData = $this->setData($this->inputBuffer);
+            $this->lastData = $this->setData($this->inputBuffer, $this->outputBuffer);
         }
         return $stopColor;
     }
-    abstract function setData(array $inputs);
+    abstract function setData(array $inputs, array $outputs = null);
     public function getData(){
         return $this->lastData;
     }
